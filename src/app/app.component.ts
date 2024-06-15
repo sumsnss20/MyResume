b885dc85584ed880d5c9261a8ae93d493d9e88cd
sumsnss20 <sumanharsha1997@gmail.com>
2024-06-15 17:45:51 +0530

    Del

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ResumeComponent } from './resume/resume.component';
 import { Resume24Component } from './resume24/resume24.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ResumeComponent,Resume24Component],
+  imports: [RouterOutlet,Resume24Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
